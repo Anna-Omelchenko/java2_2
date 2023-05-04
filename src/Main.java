97bf32641517a082e12ef2443cb076942cade11b
Anna Omelchenko <anna.g.omelchenko@gmail.com>
2023-05-04 21:21:27 +0300

    Code update

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         int initialAccountAmount = 230;
-        int accountRefill = 10;
-        byte minAdditionForBonus = 100;
-        int bonusCount = accountRefill / minAdditionForBonus;
-        boolean refillIsSufficient = bonusCount >= 1;
-        int finalAccountAmount = refillIsSufficient ? initialAccountAmount + accountRefill + bonusCount : initialAccountAmount + accountRefill;
+        int accountRefill = 1007;
+        byte bonusUnit = 100;
+        short minAccountRefillFor_Bonus = 1000;
+        int bonusCount = accountRefill / bonusUnit;
+        boolean ifBonusAdded = accountRefill >= minAccountRefillFor_Bonus;
+        int finalAccountAmount = ifBonusAdded ? initialAccountAmount + accountRefill + bonusCount : initialAccountAmount + accountRefill;
         System.out.println("Остаток на счете: " + finalAccountAmount);
     }
 }
